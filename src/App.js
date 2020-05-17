944d03db4491534d8b29059e22fe4cb751541579
sujoshk <sujoshk@gmail.com>
2020-05-18 01:16:13 +0530

    added redux, and impolemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
-import {auth} from './firebase/firebase.utils';
-
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;  
 
   componentDidMount() {
-    this.unsubscribeFromAuth= auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
-    })
+    const {setCurrentUser} = this.props;
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+          })
+        });
+        
+      }
+
+      setCurrentUser({userAuth});
+      
+    });
   }
 
   componentWillUnmount() {
@@ -32,7 +46,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -44,4 +58,7 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null,mapDispatchToProps)(App);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -14,9 +14,6 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-
-        
-
     </div>
 );
 
